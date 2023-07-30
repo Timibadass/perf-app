a22db3c591000aa9d8fed093e0e4653deff9526f
timibadass <omoyenitimi@gmail.com>
2023-07-30 15:31:02 +0100

    init

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    Hello
+    <section class="article__section">
+      <ArticleCard
+        v-for="(article, index) in articles"
+        :key="index"
+        :article="article"
+      />
+    </section>
+  </main>
+</template>
+<script setup>
+  import ArticleCard from "@/components/ArticleCard.vue";
+  import { articles } from "@/utils/articles.json";
+</script>
+<style lang="scss" scoped>
+  .article {
+    &__section {
+    }
+  }
+</style>

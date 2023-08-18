49b409b7f77d01609749dff58cbf69f1e9cd715b
timibadass <omoyenitimi@gmail.com>
2023-08-18 09:55:24 +0100

    init

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    Hello
+    <h1 class="article__heading">Hello</h1>
     <section class="article__section">
       <ArticleCard
         v-for="(article, index) in articles"
@@ -15,8 +15,18 @@
   import { articles } from "@/utils/articles.json";
 </script>
 <style lang="scss" scoped>
+  @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
   .article {
+    &__heading {
+      font-weight: 700;
+    }
     &__section {
+      font-family: "Open Sans", sans-serif;
+      @media screen and (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        gap: 16px;
+      }
     }
   }
 </style>
---
+++ components/ArticleCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="article__div">
+    <img class="article__img" :src="article.urlToImage" :alt="article.title" />
+    <div class="article__item-div">
+      <p>{{ article.title }}</p>
+      <p>{{ article.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    article: {
+      type: Object,
+      required: true,
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .article {
+    &__div {
+      box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+      border-radius: 6px;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+    &__img {
+      width: 100%;
+      object-fit: cover;
+      height: 180px;
+      border-top-left-radius: 6px;
+      border-top-right-radius: 6px;
+      transition: all 1s ease-in-out;
+      &:hover {
+        object-fit: fill;
+      }
+    }
+    &__item-div {
+      padding: 0 8px;
+    }
+  }
+</style>

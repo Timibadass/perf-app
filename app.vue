c6d14e11646787c5ed325b73353f3dab7733c8a1
Timi <oblacishere@gmail.com>
2023-09-25 09:01:30 +0100

    turn off devtools

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <h1 class="article__heading">Hello</h1>
+    <h1 class="article__heading">Top News</h1>
     <section class="article__section">
       <ArticleCard
         v-for="(article, index) in articles"
@@ -16,15 +16,18 @@
 </script>
 <style lang="scss" scoped>
   @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
   .article {
     &__heading {
+      font-family: 'Lato', sans-serif;
       font-weight: 700;
+      text-align: center;
     }
     &__section {
       font-family: "Open Sans", sans-serif;
       @media screen and (min-width: 768px) {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
         gap: 16px;
       }
     }
